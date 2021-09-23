69f719c4d666df84e6de07210923b238b1788986
fatimarida <ridafatima477@gmail.com>
2021-09-23 22:27:30 +0500

    added migrations

---
+++ database/migrations/2021_09_23_161409_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('category_id')->index();
+            $table->string('name')->index();
+//            $table->string('slug')->index();
+            $table->integer('refundable_deposit')->unsigned();
+            $table->integer('rent')->unsigned();
+            $table->string('image_path');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
